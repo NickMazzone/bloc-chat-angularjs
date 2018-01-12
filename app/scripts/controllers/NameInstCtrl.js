c674b4faf40bf85411a82bc583702eec1ee75ec8
NickMazzone <Nick.mazzone@aol.com>
2018-01-12 00:05:38 -0800

    bloc-chat-5 completed

--- app/scripts/controllers/NameInstCtrl.js
+++ app/scripts/controllers/NameInstCtrl.js
@@ -2,10 +2,13 @@
     function NameInstCtrl($uibModalInstance) {
       this.username='';
       this.submit = function(){
-        $uibModalInstance.close(this.username);
+          if (/\S/.test(this.username)==false) {
+            alert("Sorry! Username must contain at least one character!");
+          } else {
+            $uibModalInstance.close(this.username);
+          }
       };
     }
-
     angular
         .module('blocChat')
         .controller('NameInstCtrl', ['$uibModalInstance', NameInstCtrl]);
--- app/scripts/services/angular-cookies.js
+++ app/scripts/services/angular-cookies.js
@@ -1,11 +1,23 @@
 (function() {
-  function BlocChatCookies ($cookies, $uibModal, NameModalCtrl) {
+  function BlocChatCookies ($cookies, $uibModal) {
     var currentUser = $cookies.get('blocChatCurrentUser');
     if (!currentUser || currentUser === '') {
-      NameModalCtrl.open;
+      var modalInstance=$uibModal.open({
+           animation: true,
+           keyboard: false,
+           controller: 'NameInstCtrl',
+           controllerAs: 'nameInstCtrl',
+           size: 'sm',
+           templateUrl: "/templates/usernameModal.html"
+       });
+       modalInstance.result.then(function(username){
+           $cookies.put('blocChatCurrentUser', username);
+           console.log(username);
+       });
       }
+    console.log(currentUser);
     }
   angular
     .module('blocChat')
-    .run(['$cookies', '$uibModal', 'NameModalCtrl', BlocChatCookies]);
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
 })();
