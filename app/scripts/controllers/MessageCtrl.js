1a21fe3a5f490b13f896126a8d3a5769601e8514
NickMazzone <Nick.mazzone@aol.com>
2018-01-16 11:57:55 -0800

    bloc chat 6 messages completed

--- app/scripts/controllers/MessageCtrl.js
+++ app/scripts/controllers/MessageCtrl.js
@@ -1,14 +1,18 @@
 (function() {
-    function MessageCtrl(Message) {
-      var allMessages=Message.allMessages;
-      allMessages.$loaded().then(
-        function(){
-          this.messages=allMessages;
-        }
-      );
+    function MessageCtrl(Message, $cookies) {
+      this.send = function(roomId){
+        var messageObject= {
+          content: this.message,
+          roomId: roomId,
+          username: $cookies.get("blocChatCurrentUser"),
+          sentAt: new Date().toISOString()
+        };
+        Message.send(messageObject);
+        this.message="";
+      };
     }
 
     angular
         .module('blocChat')
-        .controller('MessageCtrl', ['Message', MessageCtrl]);
+        .controller('MessageCtrl', ['Message', '$cookies', MessageCtrl]);
 })();
--- app/scripts/services/message.js
+++ app/scripts/services/message.js
@@ -3,13 +3,18 @@
     var Message = {};
     var ref = firebase.database().ref().child("Messages");
     var allMessages = $firebaseArray(ref);
+    this.allMessages = allMessages;
 
     Message.getByRoomId = function(roomId) {
       console.log("get messages for " + roomId);
       this.messages = $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
-      console.log(this.messages);
-      console.log(allMessages);
       return this.messages;
+    };
+
+    Message.send = function(newMessage){
+      console.log(newMessage);
+      allMessages.$add(newMessage);
+      //firebase      (username    + content)
     };
 
     return Message;
